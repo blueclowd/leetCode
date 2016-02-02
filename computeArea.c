f24b5c75fa7f8dabc4dd5aee09a8316d3fe72890
blueclowd <hover_g@hotmail.com>
2016-02-03 01:44:05 +0800

    commit

--- computeArea.c
+++ computeArea.c
@@ -1,5 +1,11 @@
 #include "functionInterface.h"
 
+
+/**
+ Find the total area covered by two rectilinear rectangles in a 2D plane.
+
+ Each rectangle is defined by its bottom left corner and top right corner as shown in the figure.
+*/
 int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
 
     // (A,B) - (C,D)   (E,F) - (G-H)
@@ -27,3 +33,4 @@ int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
     return (C-A)*(D-B) + (G-E)*(H-F) - overlap;
 
 }
+
